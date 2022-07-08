5c2bb50a0794aea8f6c7cac83749b8811e295689
jonarroh <jonarrodi99@gmail.com>
2022-07-08 11:17:21 -0500

    🛠️ probar responsive

---
+++ src/components/Jonathan.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="home-centrar lg:flex-row">
+		<div class="mt-20 text-center sm:align-middle">
+			<span class="text-gray-200 text-4xl font-bold"
+				>Jonathan Uriel Rodriguez Hernandez</span
+			>
+			<div>
+				<span class="text-gradient-v"> Sotfware Engineer </span>
+			</div>
+		</div>
+		<div class="">
+			<div class="clip-path mx-5"></div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.text-gradient-v {
+	font-size: 2.5rem;
+	color: transparent;
+	font-weight: 900;
+	background-image: linear-gradient(0deg, #f93a35, #dada62);
+	background-clip: content-box;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	background-size: 100% 200%;
+	background-position-y: 100%;
+	border-radius: 0.4rem;
+}
+
+.home-centrar {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: 70vh;
+}
+
+.clip-path {
+	clip-path: polygon(
+		25% 0%,
+		75% 0%,
+		100% 50%,
+		75% 100%,
+		25% 100%,
+		0% 50%
+	);
+
+	background-color: #f93a35;
+	width: 320px;
+	height: 300px;
+}
+
+.log {
+	border: #f93a35 solid 1px;
+}
+</style>
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,21 +1,18 @@
 <template>
 	<section>
 		<Navbar></Navbar>
+		<Jonathan></Jonathan>
 	</section>
 </template>
 
 <script setup lang="ts">
 import Navbar from '../components/Navbar.vue';
+import Jonathan from '../components/Jonathan.vue';
 </script>
 
 <style scoped>
-:root {
-	--backgroud-g: #311558;
-}
-
 section {
 	width: 100%;
 	height: 100vh;
-	background-color: rgb(19, 18, 58);
 }
 </style>

7dd418aa74062f930a107848bc4d6d7efbe50c75
jonarroh <jonarrodi99@gmail.com>
2022-07-08 10:51:30 -0500

    📨 primer commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+	<nav class="flex p-4 font-bold">
+		<div>Jonarro</div>
+		<div class="flex-1"></div>
+		<div>
+			<a href="#">Inicio</a>
+			<a href="#">Porfolio</a>
+			<a href="#">Contacto</a>
+		</div>
+	</nav>
+</template>
+
+<style>
+a {
+	@apply mx-4;
+}
+</style>
---
+++ src/views/Inicio.vue
@@ -0,0 +1,21 @@
+<template>
+	<section>
+		<Navbar></Navbar>
+	</section>
+</template>
+
+<script setup lang="ts">
+import Navbar from '../components/Navbar.vue';
+</script>
+
+<style scoped>
+:root {
+	--backgroud-g: #311558;
+}
+
+section {
+	width: 100%;
+	height: 100vh;
+	background-color: rgb(19, 18, 58);
+}
+</style>
